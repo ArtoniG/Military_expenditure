4ef8fc1d87beca8c0dafe67703b860212db81cab
ArtoniG <gui_artoni@hotmail.com>
2019-11-07 14:17:30 -0300

    adicionado objetos que armazenam o número de países e seus respectivos nomes

--- code.R
+++ code.R
@@ -2,5 +2,9 @@ library(tidyverse)
 
 dados <- read.csv("~/Downloads/2sem19/ME415/dados/analise2/Military Expenditure.csv", header = TRUE, stringsAsFactors = FALSE)
 
-no.países <- apply(dados[,5:ncol(dados)], 2, function(x) table(is.na(x))[["FALSE"]])
-plot(no.países, type = "l")
+# ARMAZENA O NÚMERO QUE PAÍSES QUE INVESTIRAM
+no.paises <- apply(dados[,5:ncol(dados)], 2, function(x) table(is.na(x))[["FALSE"]])
+plot(no.paises, type = "l")
+
+# ARMAZENA QUAIS FORAM O PAÍSES QUE INVESTIRAM
+countries <- apply(dados[,5:ncol(dados)], 2, function(x) dados$Name[!is.na(x)])
