931bc9f960f476b0087c3e52a73a7c6cb423fb8c
ArtoniG <gui_artoni@hotmail.com>
2019-11-11 13:45:43 -0300

    alterado o banco de dados

--- code.R
+++ code.R
@@ -1,23 +1,10 @@
-library(tidyverse)
+library(readr)
+library(ggplot2)
 
-dados <- read.csv("~/Downloads/2sem19/ME415/dados/analise2/Military Expenditure.csv", header = TRUE, stringsAsFactors = FALSE)
+dados <- read_csv("~/Downloads/Books/data/a_modern_approach_to_regression_with_r/nyc.csv", col_names = TRUE)
 
-# ARMAZENA O NÚMERO QUE PAÍSES QUE INVESTIRAM EM CADA ANO
-no.paises <- apply(dados[,5:ncol(dados)], 2, function(x) table(is.na(x))[["FALSE"]])
-#plot(no.paises, type = "l")
+attach(dados)
 
-# ARMAZENA QUAIS FORAM O PAÍSES QUE INVESTIRAM EM CADA ANO
-countries <- sapply(dados[,5:ncol(dados)], function(x) dados$Name[!is.na(x)])
+summary(dados)
 
-# ARMAZENA OS PAÍSES QUE SEMPRE INVESTIRAM
-always <- countries[[1]][countries[[1]] %in% countries[[2]]]
-for(i in 3:length(countries)){
-  always <- always[always %in% countries[[i]]]
-}
-
-# ARMAZENA O INVESTIMENTO ACUMULADO DOS PAÍSES
-acum.country <- apply(dados[,5:ncol(dados)], 1, sum, na.rm = TRUE)
-acum.country <- cbind(dados$Name, acumulate)
-
-# ARMAZENA O INVESTIMENTOS ACUMULADO POR ANO
-acum.year <- apply(dados[,5:ncol(dados)], 2, sum, na.rm = TRUE)
+by(dados, East, summary)
